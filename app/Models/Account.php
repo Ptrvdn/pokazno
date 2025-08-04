d1f8c5e8f410a3d765b2d496c4d07927eed00228
Petar Vuajdinovic <tvoj@email.com>
2025-08-04 12:38:19 +0200

    Dodao formu za kontakt

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -1,10 +1,21 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
-    //
+    protected $fillable = ['user_id', 'name', 'balance'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
